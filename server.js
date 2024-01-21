3196cec3a9babc0e3b793b2d93326387e366b726
sai sinare <saisinare19@gmail.com>
2024-01-21 10:33:34 +0530

    cors origin changed

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ dotenv.config();
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://urbanbazar.vercel.app/');
+  res.header('Access-Control-Allow-Origin', 'https://urbanbazar.vercel.app');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
