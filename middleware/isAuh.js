d3df4c624797cfb561207f8c595899a26dd980d1
sai sinare <saisinare19@gmail.com>
2024-01-22 06:53:04 +0530

    jwt token changes to the cookie validation to the query

--- middleware/isAuh.js
+++ middleware/isAuh.js
@@ -3,15 +3,13 @@ const dotenv = require("dotenv");
 dotenv.config();
 const JWT_SECRETE = process.env.JWT_SCERETE;
 const fetchuser = (req, res, next) => {
-  const token = req.cookies.token;
+  const token = req.query.token;
   if (!token) {
     return res
       .status(401)
       .json({
         message: "enter Valid Token",
         JWT: JWT_SECRETE,
-        token: token,
-        cookies: req.cookies,
       });
   }
   const data = jwt.verify(token, JWT_SECRETE);
